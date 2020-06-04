8243f0cca7bc5c1f87f4ac28cb626b49766a58a7
Mohammad Aziz <aziznitham@gmail.com>
2020-06-04 11:44:53 +0530

    Add files via upload

---
+++ rangeandshape.py
@@ -0,0 +1,5 @@
+import numpy as np
+A=np.arange(5) #arange function takes value and gives range from 0 that number
+print(A)
+B=np.arange(15).reshape(3,5) #reshape breaks into number of rows and numbers in a rows
+print(B)
