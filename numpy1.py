0089973101c9addb18779ebb1bfda6bbdaf6b5b7
Mohammad Aziz <aziznitham@gmail.com>
2020-06-04 11:23:15 +0530

    Add files via upload

---
+++ numpy1.py
@@ -0,0 +1,10 @@
+import numpy as np
+A=np.array([[1,2,3],[2,3,4],[3,4,5]], dtype=complex)
+print(A)
+print("Print zero matrix")
+B=np.zeros((3,3)) #3 rows and 3 columns
+print(B)
+
+print("Print one's matrix")
+C=np.ones((3,4))
+print(C)
