5123506159d2b63a305ff13e5dd0edb542c8dcd2
Mohammad Aziz <aziznitham@gmail.com>
2020-06-05 19:28:36 +0530

    Add files via upload

---
+++ universal_function.py
@@ -0,0 +1,10 @@
+import numpy as np
+a=np.pi
+print(a)
+x=np.array([0,np.pi/2,np.pi])
+y=np.sin(x)
+print("value of sin(0), sin(pi/2), sin(pi) is {}".format(y))
+y1=np.cos(x)
+print("Value of cos(0), cos(pi/2),cos(pi) is {}: ".format(y1))
+y2=np.tan(x)
+print("value of tan(0), tan(pi/2), tan(pi) is {}: ".format(y2))
